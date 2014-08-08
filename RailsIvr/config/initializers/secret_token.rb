9f3d86481eded9cabdfd950f3a4c167ef0c8c897
Kevin Glinski <kevin.glinski@inin.com>
2014-08-08 10:07:17 -0400

    adding base app

---
+++ RailsIvr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsIvr::Application.config.secret_key_base = 'd270705b0717815d15aaa4f0d43fcd4537b466da3fdcebe430819adf027f7db0453357ecdc5a4d11ded8d516c38fe85674db1be376f34ec265f7ecc0d322bd1d'
